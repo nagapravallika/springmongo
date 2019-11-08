3c114372d02b5a67b08f12bb389670d54341bc4d
nagapravallika <nagapravallikaraja@gmail.com>
2019-11-08 18:22:44 +0530

    controller commited

--- src/main/java/com/df/springmongo/controlller/BookController.java
+++ src/main/java/com/df/springmongo/controlller/BookController.java
@@ -4,9 +4,6 @@ package com.df.springmongo.controlller;
 //import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.web.bind.annotation.DeleteMapping;
-//import org.springframework.web.bind.annotation.GetMapping;
-//import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -30,24 +27,5 @@ public class BookController
 		return "added book with id: ";
 		//+ book.getId();
 	}
-	
-//	@GetMapping("/findAllBooks")
-//	public List<Book> getBooks()
-//	{
-//		return repository.findAll();
-//	}
-//	
-//	@GetMapping("/findAllBooks/{id}")
-//	public Optional<Book> getBook(@PathVariable int id)
-//	{
-//		return repository.findById(id);
-//	}
-//	
-//	@DeleteMapping("/delete/{id}")
-//	public String deletBook(@PathVariable int id)
-//	{
-//		repository.deleteById(id);
-//		return "book deleteed with id : "+ id;
-//	}
 }
 
